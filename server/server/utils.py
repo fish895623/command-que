4e9af930f89a1f0178a921ca68e320cfb5409fef
fish895623 <dan990429@gmail.com>
2021-12-17 11:14:10 +0900

    Add pydocstring

--- server/server/utils.py
+++ server/server/utils.py
@@ -2,7 +2,20 @@ import subprocess
 
 
 class CommandUtilities:
+    """
+    Utils to run command by Python-daemon
+    """
+
     def __init__(self, cmd=str, encoding="UTF-8", timeout=None) -> None:
+        """
+        :param cmd: Command lines to run
+            ex) wget __file_loc__
+        :param encoding: set encoding on output
+            default_val = "UTF-8"
+        :param timeout: timeout until command finish
+
+        :return: None
+        """
         self.command = cmd.split()
         self.timeout = timeout
         self.encoding = encoding
